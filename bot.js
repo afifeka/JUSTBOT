8413a0a567f7ca8dbd76407cce05fb6da76794ee
afifeka <37196374+afifeka@users.noreply.github.com>
2018-03-26 17:36:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ bot.on("message", function(message) {
 
   switch (args[0]) {
     case "ping":
-        message.channel.send("Command Tidak Tersedia!");
+      message.author.ping
       break;
 
       break;
